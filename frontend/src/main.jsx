03e442f187fd99ccb773c94d905d5b2b81d27c91
meetkumar.prajapati <meetkumar.prajapati@deiglobal.com>
2026-04-09 15:35:21 +0530

    URL Change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+export const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api';
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
